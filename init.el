774bc489bce36e38d60af8ccd6415ee37e86c7ef
Ari Gaskins <arigaskins.tech@gmail.com>
2023-09-08 11:09:03 -0400

    fix startup bugs

--- init.el
+++ init.el
@@ -1,11 +1,14 @@
 ;; Emacs Configurations
 
 (setq inhibit-startup-message t) ; don't show splash screen at startup
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; full screen at startup
 
 (scroll-bar-mode -1) ; disable scrollbar
 
 (add-to-list 'custom-theme-load-path "~/.config/emacs/themes") ; add theme path
-(load-theme 'dracula t) ; load dracula theme 
+(load-theme 'dracula t) ; load dracula theme
+
+(add-to-list 'load-path "~/.config/emacs/init.el") ; load init.el
 
 ;; Org-Mode Configurations
 
@@ -17,4 +20,6 @@
 
 (setq org-adapt-indentation t
       org-hide-leading-stars t
-      org-odd-levels-only t) ;; set adapt indentation w/ clean stars
+      org-odd-levels-only t) ; set adapt indentation w/ clean stars
+
+(setq org-agenda-files (directory-files-recursively "~/Org/" "\\.org$")) ; add Org directory to agenda view
